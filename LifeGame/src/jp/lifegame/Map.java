40d3f36d480e9647055885ad724bc74afa55e949
firstforest <firstforest@hermes>
2011-10-04 12:53:33 +0900

    energyの種類を複数に。（途中）

--- LifeGame/src/jp/lifegame/Map.java
+++ LifeGame/src/jp/lifegame/Map.java
@@ -52,7 +52,8 @@ public class Map {
 	public int redEne(int x, int y, int ene) {
 		if (0<=x && x<width && 0<=y && y<height) {
 			MapChip map = maps[x + y * width];
-			return map.redEne(ene);
+			//TODO energyの種類に未対応
+			return map.redEne(0,ene);
 		}
 		return 0;
 	}
@@ -60,67 +61,74 @@ public class Map {
 	public void setEne(int x, int y, int ene) {
 		if (0<=x && x<width && 0<=y && y<height) {
 			MapChip map = maps[x + y * width];
-			map.setEne(ene);
+			//TODO energyの種類に未対応
+			map.setEne(0,ene);
 		}
 	}
 
 
 	private class MapChip {
-		private int energy;
+		private int[] energy;
 		private int x,y;
 		private static final int SIZE = 4;
+		private static final int ENE_NUM = 2;
 
 		public MapChip(int x, int y) {
-			energy = 0;
+			energy = new int[ENE_NUM];
+			for (int e : energy) {
+				e = 0;
+			}
 			this.x = x;
 			this.y = y;
 		}
 
 		public void genEne() {
-			int ene=0;
-			int tx, ty;
+			for (int k=0; k<ENE_NUM; k++) {
+				int ene=0;
+				int tx, ty;
 
-			for (int i=-1; i<2; i++) {
-				for (int j=-1; j<2; j++) {
-					if (i==0 && j==0 || (i != 0 && j != 0)) continue;
-					tx = x + i;
-					ty = y + j;
-					if (0<=tx && tx<width && 0<=ty && ty<height) {
-						MapChip map = maps[tx + ty * width];
-						ene += map.getEne();
+				for (int i=-1; i<2; i++) {
+					for (int j=-1; j<2; j++) {
+						if (i==0 && j==0 || (i != 0 && j != 0)) continue;
+						tx = x + i;
+						ty = y + j;
+						if (0<=tx && tx<width && 0<=ty && ty<height) {
+							MapChip map = maps[tx + ty * width];
+							ene += map.getEne(k);
+						}
 					}
 				}
-			}
 
-			ene /= 4;
-			this.energy = (energy < ene) ? ene : energy;
+				ene /= 4;
+				this.energy[k] = (this.energy[k] < ene) ? ene : this.energy[k];
+			}
 		}
 
-		public int redEne(int x) {
-			if (x <= energy) {
-				energy -= x;
+		public int redEne(int n, int x) {
+			if (x <= energy[n]) {
+				energy[n] -= x;
 				return x;
 			} else {
-				int t = energy;
-				energy = 0;
+				int t = energy[n];
+				energy[n] = 0;
 				return t;
 			}
 		}
 
-		public int getEne() {
-			return energy;
+		public int getEne(int n) {
+			return energy[n];
 		}
 
-		public void setEne(int ene) {
-			this.energy = ene;
+		public void setEne(int n, int ene) {
+			this.energy[n] = ene;
 		}
 
 		public void draw(Graphics g) {
-			if (energy > 200) {
+			if (energy[0] > 200) {
 				g.setColor(new Color(255,255,0));
-			} else if (energy > 100) {
+			} else if (energy[0] > 100) {
 				g.setColor(new Color(255, 255, 128));
-			} else if (energy > 0) {
+			} else if (energy[0] > 0) {
 				g.setColor(new Color(255, 255, 200));
 			} else {
 				g.setColor(Color.WHITE);
